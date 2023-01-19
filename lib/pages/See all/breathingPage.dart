294874f160e0fabc29aff07402d7ae1ab9bf74d6
dangakeri <dangakeri254@gmail.com>
2023-01-20 00:26:09 +0300

    text

--- lib/pages/See all/breathingPage.dart
+++ lib/pages/See all/breathingPage.dart
@@ -1,3 +1,4 @@
+import 'package:Warriors/consts/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class BreathingPage extends StatelessWidget {
@@ -16,6 +17,9 @@ class BreathingPage extends StatelessWidget {
             flexibleSpace: FlexibleSpaceBar(
               background: Container(
                 decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(60),
+                      bottomRight: Radius.circular(60)),
                   image: DecorationImage(
                     fit: BoxFit.cover,
                     image: AssetImage(
@@ -25,9 +29,7 @@ class BreathingPage extends StatelessWidget {
                 ),
                 child: Column(
                   children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * .1,
-                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * .1),
                     const Padding(
                       padding: EdgeInsets.symmetric(horizontal: 20),
                       child: Center(
@@ -45,18 +47,91 @@ class BreathingPage extends StatelessWidget {
           ),
           SliverList(
               delegate: SliverChildListDelegate([
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Container(
-                height: MediaQuery.of(context).size.height * .11,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
+            SizedBox(height: MediaQuery.of(context).size.height * .05),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Meditations',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
               ),
             ),
+            MeditationWidget(),
           ]))
         ],
+      ),
+    );
+  }
+}
+
+class MeditationWidget extends StatelessWidget {
+  const MeditationWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        height: MediaQuery.of(context).size.height * .09,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.black,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            SizedBox(width: 10),
+            Container(
+              height: 60,
+              width: 60,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/breathing.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(width: 10),
+            Column(
+              children: [
+                SizedBox(height: 15),
+                const Text(
+                  'Gentle tools for sleep',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 3),
+                Row(
+                  children: const [
+                    Text(
+                      '30 Min',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      'Daniel Gakeri',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Spacer(),
+            Icon(Icons.lock),
+            SizedBox(width: 5),
+          ],
+        ),
       ),
     );
   }
